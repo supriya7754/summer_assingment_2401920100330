3c6252db21c4ab77d864e1a3050e2b64b4421560
supriya7754 <supriyagaur29@gmail.com>
2026-06-28 23:08:12 +0530

    Create question3.cpp

---
+++ week 1/day_3/question3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int n=height.size();
+        int left=0;
+        int right=n-1;
+        int maxarea=0;
+        while(left<right){
+            int h=min(height[left],height[right]);
+            int width=right-left;
+            int area=h*width;
+            maxarea=max(maxarea,area);
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxarea;
+    }
+};
